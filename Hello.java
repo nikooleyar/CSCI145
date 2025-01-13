5da7ddbb952387c2f37bada945c0895b79613c8e
Niko Oleyar <oleyarn@wwu.edu>
2025-01-13 10:15:24 -0800

    First commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.println("This is another line.");
+        System.out.printf("%s%n%n%d%n%s", "FORMAT", 10, "new thing");
+    }
+}
