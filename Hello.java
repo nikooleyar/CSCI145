1c30114b309a1c622000732d9490a5874c22a2ca
Niko Oleyar <oleyarn@wwu.edu>
2025-01-13 10:21:28 -0800

    removed "new thing" in one of the lines

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
         System.out.println("This is another line.");
-        System.out.printf("%s%n%n%d%n%s", "FORMAT", 10, "new thing");
+        System.out.printf("%s%n%n%d%n%s", "FORMAT", 10);
     }
 }
