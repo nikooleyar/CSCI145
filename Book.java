01bcadf1aa91b591b0acbe9f9a9b067dda474eb2
nikooleyar <chantingcomet18@gmail.com>
2025-01-31 10:54:33 -0800

    Updates for Book.java example

--- Book.java
+++ Book.java
@@ -1,19 +1,29 @@
 class Book {
-    String title;
-    int year;
-    String author;
+	String title;
+	int year;
+	String author;
+	static int totalBooks = 0;
 
-    Book(String title, int year, String author) {
+	Book(String title, int year, String author) {
+		this.title = title;
+		this.year = year;
+		this.author = author;
+		totalBooks++;
+	}
 
-        this.title = title;
-        this.year = year;
-        this.author = author;
-        }
-        
-        void displayInfo() {
-            System.out.println("Title: " + title);
-            System.out.println("Year: " + year);
-            System.out.println("Author: " + author);
-        }
-    }
-    
+	void displayInfo() {
+		System.out.println("Title: " + title);
+		System.out.println("Publication Year: " + year);
+	}
+
+	String getBookString() {
+		String bookString = "title:" + title + 
+			"\nyear:" + year + 
+			"\nauthor:" + author + "\n";
+		return bookString;
+	}
+
+	static boolean compareTitle(Book book1, Book book2) {
+		return book1.title.equals(book2.title);
+	}
+}
