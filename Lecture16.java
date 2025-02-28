ec3ad3608c03b9add19ced26201f96c6ffd3c81a
nikooleyar <chantingcomet18@gmail.com>
2025-02-28 12:15:54 -0800

    Recursion continues with fibonacci seq but its almost empty lol

---
+++ Lecture16.java
@@ -0,0 +1,7 @@
+public class Lecture16 {
+    public static void main(String[] args) {
+        //a fibonacci number is the sum of the two proceeding numbers in the sequence
+        //fibonacci (5) = 5
+        //fibonacci (6) = 8
+    }
+}
