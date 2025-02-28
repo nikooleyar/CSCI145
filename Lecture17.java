08f66a8088b9622300b9e3145efd052c81bb59e8
nikooleyar <chantingcomet18@gmail.com>
2025-02-28 12:17:25 -0800

    Binary Search + Merge Sort recurisve methods

---
+++ Lecture17.java
@@ -0,0 +1,19 @@
+public class Lecture17 {
+    //Binary Search
+
+    //is an algorithm used to find the position of a target value within a sorted array
+    //kinda like opening a book in the middle and then going to half with whatever page number you want
+
+    //int binarySearch(int[] A, int key, int i, int j)
+    //sorted array
+    //value we're looking for
+    //i should be 0 and j should be the index of length of array
+    //should return -1 if target not in array
+
+    //Sorting
+
+    //Divide and Conquer
+    //Merge Sort
+
+    //divide the problem into sub problems and then solve recursively 
+}
