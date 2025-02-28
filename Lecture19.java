003d0ea4d467d099e8525d7a2e333585f682ced2
nikooleyar <chantingcomet18@gmail.com>
2025-02-28 12:18:10 -0800

    Linked Lists continued (doubly linked)

---
+++ Lecture19.java
@@ -0,0 +1,9 @@
+public class Lecture19 {
+    //LINKED LISTS CONTINUED
+
+    //Doubly Linked List
+    //Where each node contains a value, a reference to its successor, AND a reference to its predecessor
+    //allows to go forwards and backwards in the linked list
+
+
+}
